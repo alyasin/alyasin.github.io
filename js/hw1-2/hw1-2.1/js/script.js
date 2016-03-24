b890e988a0fb9c90b679d5149bd93a298fa6ffb5
Aleksey Yasinskiy <a_yasinskiy@ukr.net>
2016-03-24 13:15:48 +0200

    fix

--- js/hw1-2/hw1-2.1/js/script.js
+++ js/hw1-2/hw1-2.1/js/script.js
@@ -1,31 +1,35 @@
 
 do { var number = prompt('Введите целое число') } 
-while (isNaN(number) || ((number % 1) != 0));
+while (isNaN(number) || ((number % 1) !== 0));
 
 do { var extent = prompt('Введите показатель степени, целое число') } 
-while (isNaN(extent) || ((extent % 1) != 0));
+while (isNaN(extent) || ((extent % 1) !== 0));
 
-if (number == -0) {number = Math.abs(number)};
-if (extent == -0) {extent = Math.abs(extent)};
+if (number == -0) {
+  number = Math.abs(number)
+};
+if (extent == -0) {
+  extent = Math.abs(extent)
+};
 
 console.log(pow(number, extent));
 
 function pow (number, extent) {
   
-  var numberInExtent = 1; //  if ((number != 0) && (extent == 0)) 
+  var numberInExtent = 1;
   
   if ((number == 0) && (extent <= 0)) {
     numberInExtent = 'Не определён';
   };
   
   if ((number != 0) && (extent < 0)) {
-    for (var i = extent; i < 0; i++) {
+    for (i = extent; i < 0; i++) {
       numberInExtent = numberInExtent * number;
     };
     numberInExtent = 1 / numberInExtent;
   };
   
-  for (var i = 0; i < extent; i++) {
+  for (i = 0; i < extent; i++) {
     numberInExtent = numberInExtent * number;
   };
 
--- js/hw1-2/hw1-2.2/js/script.js
+++ js/hw1-2/hw1-2.2/js/script.js
@@ -1,23 +1,23 @@
 var nameArr = [];
 
 for (var i = 0; i < 5; i++) {
-  nameArr[i] = prompt('Insert Name');
-};
+  nameArr[i] = prompt('Insert Name №' + (i + 1));
+}
 
 var user = prompt('Insert user Name');
 
 var key = false;
 
-for (var i = 0; i < nameArr.length; i++) {
+for (i = 0; i < nameArr.length; i++) {
   if (nameArr[i] == user) {
     key = true;
     i = nameArr.length;
   }
-};
+}
 
 if (key == true) {
   alert(user + ', вы успешно вошли');
 } else {
   alert('Error')
-};
+}
 
