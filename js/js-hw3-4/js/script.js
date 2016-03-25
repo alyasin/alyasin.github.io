f9cf4b0507fd01ec830afae7d1b0673b4fd817ad
Aleksey Yasinskiy <a_yasinskiy@ukr.net>
2016-03-25 21:38:08 +0200

    1

--- js/js-hw3-4/js/script.js
+++ js/js-hw3-4/js/script.js
@@ -3,10 +3,11 @@ var test = {
   testBody: document.body,
   testForm: document.createElement('form'),
   testHeader: document.createElement('h2'),
-  fragment: document.createDocumentFragment(),
-  testSection: document.createElement('form'),
-  testAnswerLabel: document.createElement('label'),
-  testAnswerInput: document.createElement('input'),
+//  fragment: document.createDocumentFragment(),
+//  testQuestion: document.createElement('form'),
+//  testAnswerLabel: document.createElement('label'),
+//  testAnswerInput: document.createElement('input'),
+//  testAnswerText: document.createElement('p'),
   testButton: document.createElement('button'),
   
   createForm: function() {
@@ -14,35 +15,49 @@ var test = {
     this.testForm.classList.add('testForm');
   },
   
-  createHeader: function() {
+  createHeader: function(text) {
     this.testForm.appendChild(this.testHeader);
     this.testHeader.classList.add('header');
-    this.testHeader.innerHTML = 'Тест по программированию';
+    this.testHeader.innerHTML = text;
   },
   
-  createSection: function() {
-    for ( var i = 1; i < 4; i++) {
-      this.testForm.appendChild(this.testSection);
-      this.testSection.className = 'question_' + i;
-      this.testSection.innerHTML = '<h2>' + i + '. Вопрос №' + i + '</h2>';
-      for (var j = 1; j < 4; j++) {
-        this.testSection.appendChild(this.testAnswerInput);
-        this.testAnswerInput.setAttribute('type', 'checkbox');
-        this.testAnswerInput.setAttribute('id', 'answer_' + i + '_' + j);
-        this.testSection.appendChild(this.testAnswerLabel);
-        this.testAnswerLabel.setAttribute('for', 'answer_' + i + '_' + j);
-        this.testAnswerLabel.innerHTML = 'Вариант ответа №' + j;  
-//        debugger;
+  createQuestion: function(maxQuestion, maxAnswer) {
+    for ( var question = 1; question < maxQuestion + 1; question++) {
+      
+      var testQuestion = document.createElement('form');
+      this.testForm.appendChild(testQuestion);
+      testQuestion.className = 'question ' + 'question_' + question;
+      testQuestion.innerHTML = '<h2>' + question + '. Вопрос №' + question + '</h2>';
+      
+      for (var answer = 1; answer < maxAnswer + 1; answer++) {
+        
+        var testAnswerLabel = document.createElement('label');
+        testQuestion.appendChild(testAnswerLabel);
+        testAnswerLabel.className = 'answer ' + 'answer_' + question + '_' + answer;
+        
+        var testAnswerInput = document.createElement('input');
+        testAnswerLabel.appendChild(testAnswerInput);
+        testAnswerInput.className = 'checkbox';
+        testAnswerInput.setAttribute('type', 'checkbox');
+        testAnswerInput.setAttribute('id', 'answer_' + question + '_' + answer);
+        
+        var testAnswerText = document.createElement('p');
+        testAnswerLabel.appendChild(testAnswerText);
+        testAnswerText.innerHTML = 'Вариант ответа №' + answer;  
       } 
     }
-    this.testForm.appendChild(this.testButton)
   },
-  createAnswer: function(i, j) {
+  
+  createButton: function(text) {
+    this.testForm.appendChild(this.testButton);
+    this.testButton.className = 'testButton';
+    this.testButton.innerHTML = '<h2>' + text + '</h2>';
   },
 }
 
 test.createForm();
-test.createHeader();
-test.createSection();
+test.createHeader('Тест по программированию');
+test.createQuestion(3, 3);
+test.createButton('Проверить мои результаты')
 
 
