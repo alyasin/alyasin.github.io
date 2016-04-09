5a05b7cff6b9f3f38b3bd3770827b1b2ceb81783
Aleksey Yasinskiy <a_yasinskiy@ukr.net>
2016-04-10 01:51:53 +0300

    fix2

--- js/hw5-6/js/script.js
+++ js/hw5-6/js/script.js
@@ -1,91 +1,102 @@
+var timer = (function(){
 
-var start = document.querySelector('.start');
-var pause = document.querySelector('.pause');
+  var start = document.querySelector('.start');
+  var pause = document.querySelector('.pause');
+  var clear = document.querySelector('.clear');
+  var timerId = -1;
+  var dStart = 0;
+  var hhStart;
+  var mmStart;
+  var ssStart;
+  var mssStart;
 
-var timerId = -1;
-var dStart;
-var hhStart;
-var mmStart;
-var ssStart;
-var mssStart;
+  document.getElementById("time").innerHTML = '00:00:00';
+  document.getElementById("milliseconds").innerHTML = 0;
 
-clearInt(timerId, 'clear');
+  start.addEventListener('click', function(){
 
-pause.setAttribute('id', 'displayNone');
+    timerId = setInterval(myTimer, 1);
 
-function timer() {
+    start.setAttribute('id', 'displayNone');
+    pause.setAttribute('id', 'displayBlock');
 
-  timerId = setInterval(myTimer ,1);
+    document.querySelector('.start').innerHTML = 'Cont..';
+  });
 
-  start.setAttribute('id', 'displayNone');
-  document.querySelector('.start').innerHTML = 'Cont..';
-  pause.setAttribute('id', 'displayBlock');
-}
+  pause.addEventListener('click', function(){
 
-function myTimer() {
+    if (timerId > -1) {
+      clearInterval(timerId);
+    }
 
-  var d = new Date();
-  var hh = new Date().getHours();
-  var mm = new Date().getMinutes();
-  var ss = new Date().getSeconds();
-  var mss = new Date().getMilliseconds()
+    pause.setAttribute('id', 'displayNone');
+    start.setAttribute('id', 'displayBlock');
+  });
 
-  if (dStart == 0) {
+  clear.addEventListener('click', function(){
 
-    hhStart = hh;
-    mmStart = mm;
-    ssStart = ss;
-    mssStart = mss;
-    dStart = d;
-  }
+    if (timerId > -1) {
+      clearInterval(timerId);
+    }
 
-  hh = hh - hhStart;
-  mm = mm - mmStart;
-  ss = ss - ssStart;
-  mss = mss - mssStart;
-
-  d.setHours(hh, mm, ss, mss);
-
-  var timeString = d.toLocaleTimeString();
-
-  if (timeString.length < 8) {
-
-    timeString = '0' + timeString;
-  }
-
-  document.getElementById("time").innerHTML = timeString;
-
-  var milliseconds = d.getMilliseconds();
-
-  if (milliseconds < 10) {
-
-    milliseconds = '0' + milliseconds
-  }
-
-  if (milliseconds < 100) {
-
-    milliseconds = '0' + milliseconds
-  }
-
-  document.getElementById("milliseconds").innerHTML = milliseconds;
-}
-
-function clearInt(timerId, button) {
-
-  if (timerId > -1) {
-
-    clearInterval(timerId);
-  }
-
-  pause.setAttribute('id', 'displayNone');
-  start.setAttribute('id', 'displayBlock');
-
-  if (button === 'clear'){
+    pause.setAttribute('id', 'displayNone');
+    start.setAttribute('id', 'displayBlock');
 
     document.querySelector('.start').innerHTML = 'Start';
     document.getElementById("time").innerHTML = '00:00:00';
     document.getElementById("milliseconds").innerHTML = 0;
 
     dStart = 0;
+  });
+
+  function myTimer() {
+
+    var d = new Date();
+    var hh = new Date().getHours();
+    var mm = new Date().getMinutes();
+    var ss = new Date().getSeconds();
+    var mss = new Date().getMilliseconds()
+
+    if (dStart == 0) {
+
+      hhStart = hh;
+      mmStart = mm;
+      ssStart = ss;
+      mssStart = mss;
+      dStart = d;
+    }
+
+    hh = hh - hhStart;
+    mm = mm - mmStart;
+    ss = ss - ssStart;
+    mss = mss - mssStart;
+
+    d.setHours(hh, mm, ss, mss);
+
+    var timeString = d.toLocaleTimeString();
+
+    if (timeString.length < 8) {
+
+      timeString = '0' + timeString;
+    }
+
+    document.getElementById("time").innerHTML = timeString;
+
+    var milliseconds = d.getMilliseconds();
+
+    if (milliseconds < 10) {
+
+      milliseconds = '0' + milliseconds
+    }
+
+    if (milliseconds < 100) {
+
+      milliseconds = '0' + milliseconds
+    }
+
+    document.getElementById("milliseconds").innerHTML = milliseconds;
   }
-}
+
+
+})();
+
