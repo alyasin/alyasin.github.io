2b60e447167cd6d727d647bacdb4e8a61fbfec68
Aleksey Yasinskiy <a_yasinskiy@ukr.net>
2016-04-16 19:25:31 +0300

    1

---
+++ js/hw11-12/js/script.js
@@ -0,0 +1,25 @@
+$(function () {
+
+  var html = $('#test').html();
+  var questions = [
+    {
+      question: 'Вопрос №1',
+      answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
+    },
+    {
+      question: 'Вопрос №2',
+      answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
+    },
+    {
+      question: 'Вопрос №3',
+      answer: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
+    }
+  ];
+
+  var content = tmpl(html, {
+    data: questions
+  });
+
+  $('body').append(content);
+
+});
