2b54313bc51ffb7487ba6085acc04b07fdfb69f9
Aleksey Yasinskiy <a_yasinskiy@ukr.net>
2016-04-22 19:44:37 +0300

    1

--- js/hw15-16/js/script.js
+++ js/hw15-16/js/script.js
@@ -33,5 +33,55 @@ $(function () {
         });
     });
   });
-
 });
+
+
+
+(function() {
+  function Human () {
+    this.name = 'Tom';
+    this.age = 25;
+    this.sex = 'male';
+    this.height = 180;
+    this.weight = 80;
+  }
+
+  function Worker () {};
+
+  Worker.prototype = new Human;
+
+  Worker.prototype.placeWork = 'Grant-Kohrs Ranch';
+  Worker.prototype.wages = 2000;
+  Worker.prototype.work = function () {
+    console.log('To work, the sun still high !!!');
+  };
+
+  function Student () {};
+
+  Student.prototype = new Human;
+
+  Student.prototype.studyPlace = 'UCLA';
+  Student.prototype.stipend = 1000;
+  Student.prototype.watchSeries = function () {
+    console.log("Exams are still far - it's time to watch the TV series");
+  }
+
+  var newWorker = [];
+  var newStudent = [];
+
+  for(var i = 1; i < 3; i++){
+    newWorker[i] = new Worker;
+    newStudent[i] = new Student;
+
+    console.log('newWorker[', i, '] -', newWorker[i]);
+    newWorker[i].work();
+    console.log('newStudent[', i, '] -', newStudent[i]);
+    newStudent[i].watchSeries();
+  }
+
+})();
+
+
+
+
+
